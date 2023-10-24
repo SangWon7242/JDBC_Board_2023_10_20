04b442de90d927f5fa866eb84bf00f319071f88a
SangWon7242 <gotkdqja@gmail.com>
2023-10-24 21:53:55 +0900

    DBUtil 도입하여 소스코드 다이어트

--- src/main/java/com/sbs/jdbc/Article.java
+++ src/main/java/com/sbs/jdbc/Article.java
@@ -1,5 +1,7 @@
 package com.sbs.jdbc;
 
+import java.util.Map;
+
 public class Article {
   int id;
   String regDate;
@@ -19,6 +21,14 @@ public class Article {
     this.id = id;
     this.title = title;
     this.content = content;
+  }
+
+  public Article(Map<String, Object> articleMap) {
+    this.id = (int) articleMap.get("id");
+    this.regDate = (String) articleMap.get("regDate");
+    this.updateDate = (String) articleMap.get("updateDate");
+    this.title = (String) articleMap.get("title");
+    this.content = (String) articleMap.get("content");
   }
 
   @Override
