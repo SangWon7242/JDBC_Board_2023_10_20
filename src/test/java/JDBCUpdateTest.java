60ede3fd4a17e98e379efc3046fa8084033c63d9
SangWon7242 <gotkdqja@gmail.com>
2023-10-24 20:47:53 +0900

    JDBC UPDATE 테스트

---
+++ src/test/java/JDBCUpdateTest.java
@@ -0,0 +1,61 @@
+import com.sbs.jdbc.Article;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JDBCUpdateTest {
+  public static void main(String[] args) {
+    // JDBC 드라이버 클래스 이름
+    String jdbcDriver = "com.mysql.cj.jdbc.Driver";
+
+    // 데이터베이스 연결 정보
+    String url = "jdbc:mysql://127.0.0.1:3306/text_board?useUnicode=true&characterEncoding=utf8&autoReconnect=true&serverTimezone=Asia/Seoul&useOldAliasMetadataBehavior=true&zeroDateTimeNehavior=convertToNull";
+    String username = "sbsst";
+    String password = "sbs123414";
+
+    Connection conn = null;
+    PreparedStatement pstat = null;
+
+    String sql = "UPDATE article";
+    sql += " SET updateDate = NOW()";
+    sql += ", title = '수정제목2'";
+    sql += ", `content` = '수정내용2'";
+    sql += " WHERE id = 2;";
+
+    System.out.println("sql : " + sql);
+
+    try {
+      // JDBC 드라이버 로드
+      Class.forName(jdbcDriver);
+
+      // 데이터베이스에 연결
+      conn = DriverManager.getConnection(url, username, password);
+
+      pstat = conn.prepareStatement(sql);
+
+      pstat.executeUpdate();
+
+    } catch (ClassNotFoundException e) {
+      System.out.println("드라이버 로딩 실패");
+    } catch (SQLException e) {
+      System.out.println("에러 : " + e);
+    } finally {
+      try {
+        if(pstat != null && !pstat.isClosed()) {
+          pstat.close();
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+      try {
+        if (conn != null && !conn.isClosed()) {
+          // 연결 닫기
+          conn.close();
+        }
+      } catch (SQLException e) {
+        e.printStackTrace();
+      }
+    }
+  }
+}
